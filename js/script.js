69b962473b087190e463d2bd65f50021d5efd1f9
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-11-04 11:50:11 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 const iconHamburger = document.querySelector(".header__hamburger--menu")
 const iconClose = document.querySelector(".header__nav__icon__close")
 const menuMobile = document.querySelector(".header__nav--mobile")
-let body = document.getElementsByTagName("body")
-
-
-console.log(body)
-
-
 
 iconHamburger.addEventListener("click", function(event){
    
@@ -20,6 +14,5 @@ iconClose.addEventListener("click", function(event){
     
     iconHamburger.style.display = "block"
  
-    console.log(event.view.innerWidth)
 })
 
